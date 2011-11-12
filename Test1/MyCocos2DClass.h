927d876bb9148847153b352b04530cf273d6f4dd
Sebastian Arenas <sebastian.arenas.ulloa@gmail.com>
2011-11-11 23:51:01 -0300

    ku!

---
+++ Test1/MyCocos2DClass.h
@@ -0,0 +1,16 @@
+//
+//  MyCocos2DClass.h
+//  Test1
+//
+//  Created by Sebastian Arenas Ulloa on 11-11-11.
+//  Copyright 2011 UTFSM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface MyCocos2DClass : CCLayer {
+    
+}
+
+@end
---
+++ Test1/MyCocos2DClass.m
@@ -0,0 +1,14 @@
+//
+//  MyCocos2DClass.m
+//  Test1
+//
+//  Created by Sebastian Arenas Ulloa on 11-11-11.
+//  Copyright 2011 UTFSM. All rights reserved.
+//
+
+#import "MyCocos2DClass.h"
+
+
+@implementation MyCocos2DClass
+
+@end
